a925871a2fa6a24fb4158981f58b2214dcef54a7
Amr Nageh <amr.nageh@example.com>
2026-07-19 15:19:07 +0300

    Update serverUrl to vibe3

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   // Use 10.0.2.2 for Android Emulator, or localhost for web/iOS simulator
-  static const String serverUrl = 'https://evibe2-zkp3v41l.b4a.run';
+  static const String serverUrl = 'https://vibe3-vt268p9q.b4a.run';
 }
