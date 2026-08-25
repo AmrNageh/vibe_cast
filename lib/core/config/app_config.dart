802c3fe067c4e8c2f33cc616531d1de86594198a
Amr Nageh <amr.nageh@example.com>
2026-08-25 23:15:31 +0300

    Update_app_config_with_active_Back4App_URL

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   // Using Back4App backend URL
-  static const String serverUrl = 'https://test1-iveocmv5.b4a.run';
+  static const String serverUrl = 'https://test1-wc4pdm1x.b4a.run';
 }
